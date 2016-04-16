5c883a0e4cfc23f3cf131619f4b7ed81ddbbadbd
Abdelkrime Aries <kariminfo0@gmail.com>
2016-04-16 10:58:49 +0100

    add japanese translateration

---
+++ JaTrans.js
@@ -0,0 +1,106 @@
+
+var hiraganaTrans = {
+		"あ": "a",
+        "い": "i",
+        "う": "u",
+        "え": "e",
+        "お": "o",
+        "か": "ka",
+        "き": "ki",
+        "く": "ku",
+        "け": "ke",
+        "こ": "ko",
+        "さ": "sa",
+        "し": "shi",
+        "す": "su",
+        "せ": "se",
+        "そ": "so",
+        "た": "ta",
+        "ち": "chi",
+        "つ": "tsu",
+        "て": "te",
+        "と": "to",
+        "な": "na",
+        "に": "ni",
+        "ぬ": "nu",
+        "ね": "ne",
+        "の": "no",
+        "は": "ha",
+        "ひ": "hi",
+        "ふ": "fu",
+        "へ": "he",
+        "ほ": "ho",
+        "ま": "ma",
+        "み": "mi",
+        "む": "mu",
+        "め": "me",
+        "も": "mo",
+        "や": "ya",
+        "ゆ": "yu",
+        "よ": "yo",
+        "ら": "ra",
+        "り": "ri",
+        "る": "ru",
+        "れ": "re",
+        "ろ": "ro",
+        "わ": "wa",
+        "を": "o",
+        "ん": "n",
+
+        "が": "ga",
+        "ぎ": "gi",
+        "ぐ": "gu",
+        "げ": "ge",
+        "ご": "go",
+        "ざ": "za",
+        "じ": "ji",
+        "ず": "zu",
+        "ぜ": "ze",
+        "ぞ": "zo",
+        "だ": "da",
+        "ぢ": "di",
+        "づ": "du",
+        "で": "de",
+        "ど": "do",
+        "ば": "ba",
+        "び": "bi",
+        "ぶ": "bu",
+        "べ": "be",
+        "ぼ": "bo",
+        "ぱ": "pa",
+        "ぴ": "pi",
+        "ぷ": "pu",
+        "ぺ": "pe",
+        "ぽ": "po"
+};
+
+
+function getTranslaterator(lookupTable) {
+    return function(text) {
+        var result = "";
+        //alert(lookupTable.length);
+        //if (text.length < lookupTable.length) {
+            for (var i = 0; i < text.length; i++){
+                var char = text.charAt(i);
+                if (char in lookupTable){
+                    result += lookupTable[char];
+                } else {
+                    var u = char.charCodeAt(0) - 0x0060;
+                    char = String.fromCharCode(u);
+                    if (char in lookupTable){
+                    result += lookupTable[char];
+                    } else {
+                        result += text.charAt(i);
+                    }      
+                }
+                
+            }
+            return result;
+        //} 
+        
+        //return result;
+    }
+}
+
+
+var toTrans = getTranslaterator(hiraganaTrans);
