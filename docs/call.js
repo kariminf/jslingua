9a2fc989ccc5729722a69975a8e1d72d9ec83a08
Abdelkrime Aries <kariminfo0@gmail.com>
2016-08-30 14:42:35 +0100

    fix the docs and examples

---
+++ docs/call.js
@@ -0,0 +1,6 @@
+//var path = "../dist/";
+var path = "https://unpkg.com/jslingua@latest/dist/";
+
+function addScript(name){
+  document.write('<script type="text/javascript" src="' + path + name + '"><\/script>');
+}
