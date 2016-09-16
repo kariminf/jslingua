70dd76e62b5d3054a92691d19aef22235524c41b
Abdelkrime Aries <kariminfo0@gmail.com>
2016-09-16 22:15:47 +0100

    add docs to the website

---
+++ docs/docs/api.js
@@ -0,0 +1,35 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AraInfo",
+        "AraLang",
+        "AraTrans",
+        "Info",
+        "JpnInfo",
+        "JpnLang",
+        "JpnTrans",
+        "JsLingua",
+        "Lang",
+        "Morpho",
+        "Tools",
+        "Trans"
+    ],
+    "modules": [
+        "JsLingua",
+        "Tools"
+    ],
+    "allModules": [
+        {
+            "displayName": "JsLingua",
+            "name": "JsLingua",
+            "description": "translateration of the language words"
+        },
+        {
+            "displayName": "Tools",
+            "name": "Tools",
+            "description": "The tools module"
+        }
+    ],
+    "elements": []
+} };
+});
