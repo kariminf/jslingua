39a0299971eb0e8f5d90f7e966e03998c7d335ca
Abdelkrime Aries <kariminfo0@gmail.com>
2017-07-29 21:59:37 +0100

    docs/call.js 20170729

--- docs/call.js
+++ docs/call.js
@@ -1,5 +1,5 @@
-var path = "../dist/";
-//var path = "https://unpkg.com/jslingua@latest/dist/";
+//ar path = "../dist/";
+var path = "https://unpkg.com/jslingua@latest/dist/";
 
 function addScript(name){
   document.write('<script type="text/javascript" src="' + path + name + '"><\/script>');
