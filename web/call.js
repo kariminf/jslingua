4aaea1b9e137a3a25cd7788f82317eb8d553c914
Abdelkrime Aries <kariminfo0@gmail.com>
2017-08-06 13:49:32 +0100

    when the site is on github it won't use local scripts

--- web/call.js
+++ web/call.js
@@ -1,5 +1,17 @@
+
 var path = "../dist/";
-//var path = "https://unpkg.com/jslingua@latest/dist/";
+
+{
+  var script = document.currentScript;
+  var fullUrl = script.src;
+
+  //console.log(fullUrl);
+
+  if (fullUrl.includes("github.io")){
+    path = "https://unpkg.com/jslingua@latest/dist/";
+  }
+}
+
 
 function addScript(name){
   document.write('<script type="text/javascript" src="' + path + name + '"><\/script>');
