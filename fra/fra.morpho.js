533cb811438bc2fff7ecab9b5edebfb70a968c9f
Abdelkrime Aries <kariminfo0@gmail.com>
2017-12-26 20:18:18 +0100

    french stemmer, step1 not complete

--- fra/fra.morpho.js
+++ fra/fra.morpho.js
@@ -26,7 +26,7 @@
 
   function FraMorpho() {
     Morpho.call(this, "fra");
-    Morpho.newStemmer.call(this, "porterStemmer", "French Snowball stemmr", snowballStemmer);
+    Morpho.newStemmer.call(this, "snowballFrStemmer", "French Snowball stemmr", snowballStemmer);
 
     //Morpho.addNounDeclension.call(this, "singularToPlural", singular2plural);
     g = this.g;
@@ -45,8 +45,8 @@
         {person:Person.T, number: GNumber.S, gender: Gender.F},//Elle
         {person:Person.F, number: GNumber.P}, //Nous
         {person:Person.S, number: GNumber.P},//Vous
-        {person:Person.T, number: GNumber.P, gender: Gender.M}//Ils
-        {person:Person.T, number: GNumber.P, gender: Gender.F},//Elles
+        {person:Person.T, number: GNumber.P, gender: Gender.M},//Ils
+        {person:Person.T, number: GNumber.P, gender: Gender.F}//Elles
 
     ];
   };
@@ -95,16 +95,163 @@
   //                 STEMMERS
   //=========================================================
 
-  http://snowball.tartarus.org/algorithms/french/stemmer.html
+  //http://snowball.tartarus.org/algorithms/french/stemmer.html
   const vowels = "aeiouyâàëéêèïîôûù";
 
   //TODO complete snowball stemmer
   function snowballStemmer(word) {
+
     //The word must be lower case
+    //============================
     word = word.toLowerCase();
+
     //vowel marking: considering some vowels as cosonents
-    
+    //===================================================
+    //TODO these are in conflect; fix may be using a loop rather than regexp
+    word = word.replace("qu", "qU");
+    word = word.replace(new RegExp("([" + vowels + "])([ui])([" + vowels + "])", "g"),
+    function(match, p1, p2, p3, offset, string) {
+      return p1 + p2.toUpperCase() + p3;
+    }
+    );
+    word = word.replace(new RegExp("([" + vowels + "])y", "g"), "$1Y");
+    word = word.replace(new RegExp("y([" + vowels + "])", "g"), "Y$1");
+
+
+    //mark regions
+    //============
+    let rv = "", r1 = "", r2 = "";
+    let m;
+    let reg = new RegExp("^[" + vowels + "]{2}.(.*)$");
+    if ((m = reg.exec(word)) != null) { //the word starts with two vowels
+      rv = m[1];
+    }
+    else { //The word don't start with two vowels
+      reg = new RegExp("^(par|col|tap)(.*)$");
+      if ((m = reg.exec(word)) != null) rv = m[2];//if it starts with par, col or tap
+      else { //otherwise, it's the string after the first vowel with is not a starting one
+        reg = new RegExp("^.[^" + vowels + "]*" + "[" + vowels + "]" + "(.*)$");
+        if ((m = reg.exec(word)) != null) rv = m[1];
+      }
+    }
+
+    //R1 is the region after the first non-vowel following a vowel,
+    //or the end of the word if there is no such non-vowel.
+    reg = new RegExp("^[^" + vowels + "]*[" + vowels + "]+[^" + vowels + "](.*)$");
+    if ((m = reg.exec(word)) != null) r1 = m[1];
+
+    //R2 is the region after the first non-vowel following a vowel in R1,
+    //or the end of the word if there is no such non-vowel
+    if ((m = reg.exec(r1)) != null) r2 = m[1];
+
+    console.log("rv= " + rv + ", r1= " + r1 + ", r2= " + r2);
+
+
+    //Step 1
+    let  processed = step1(word, rv, r1, r2);
+    console.log(processed);
+
+    if (processed.next) {
+      //processed = step2(processed.stem, rv);
+    }
+
+
     return word;
+  }
+
+  //Standard suffix removal
+  function step1(word, rv, r1, r2) {
+    let m, reg;
+
+    //ance   iqUe   isme   able   iste   eux   ances   iqUes   ismes   ables   istes
+    reg = new RegExp("^.*(ance|iqUe|isme|able|iste|eux|ances|iqUes|ismes|ables|istes)$");
+    if ((m = reg.exec(word)) != null) {
+      if (r2.endsWith(m[1])) return {stem:word.slice(0, -m[1].length), next:false};
+    }
+
+    //atrice   ateur   ation   atrices   ateurs   ations
+    reg = new RegExp("^.*(atrice|ateur|ation|atrices|ateurs|ations)$");
+    if ((m = reg.exec(word)) != null) {
+      if (r2.endsWith(m[1])) {
+        word = word.slice(0, -m[1].length);
+        if (!word.endsWith("ic")) return {stem:word, next:false};
+        if (r2.endsWith("ic" + m[1])) return {stem:word.slice(0, -2), next:false};
+        return {stem:word.slice(0, -1) + "qU", next:false};
+      }
+    }
+
+    //logie   logies
+    reg = new RegExp("^.*log(ie|ies)$");
+    if ((m = reg.exec(word)) != null) {
+      if (r2.endsWith("log" + m[1])) return {stem:word.slice(0, -m[1].length), next:false};
+    }
+
+    //usion   ution   usions   utions
+    reg = new RegExp("^.*u(sion|tion|sions|tions)$");
+    if ((m = reg.exec(word)) != null) {
+      if (r2.endsWith("u" + m[1])) return {stem:word.slice(0, -m[1].length), next:false};
+    }
+
+    //ence   ences
+    reg = new RegExp("^.*en(ce|ces)$");
+    if ((m = reg.exec(word)) != null) {
+      if (r2.endsWith("en" + m[1])) return {stem:word.slice(0, -m[1].length) + "t", next:false};
+    }
+
+    //ement   ements
+    reg = new RegExp("^.*(ement|ements)$");
+    if ((m = reg.exec(word)) != null && rv.endsWith(m[1])) {
+        //delete if in RV
+        word = word.slice(0, -m[1].length);
+        //if preceded by iv, delete if in R2 (and if further preceded by at, delete if in R2)
+        if (r2.endsWith("iv" + m[1])) {//here word.endsWith("iv") ommited since r2 is the end
+          word = word.slice(0, -2);
+          if (r2.endsWith("ivat" + m[1])) return {stem:word.slice(0, -2), next:false};
+          return {stem:word, next:false};
+        }
+        //if preceded by eus, delete if in R2, else replace by eux if in R1, otherwise,
+        if (word.endsWith("eus")) {
+          if (r2.endsWith("eus" + m[1])) return {stem:word.slice(0, -3), next:false};
+          if (r2.endsWith("eus" + m[1])) return {stem:word.slice(0, -1) + "x", next:false};
+        }
+        //if preceded by abl or iqU, delete if in R2, otherwise,
+        if ( (new RegExp("^.*(abl|iqU)" + m[1] + "$")).test(r2)) {
+          return {stem:word.slice(0, -3), next:false};
+        }
+        //if preceded by ièr or Ièr, replace by i if in RV
+        if ( (new RegExp("^.*[iI]èr" + m[1] + "$")).test(rv)) {
+          return {stem:word.slice(0, -3) + "i", next:false};
+        }
+        //otherwise
+        return {stem:word, next:false};
+    }//end ement(s)
+
+    //ité   ités
+    reg = new RegExp("^.*(ité|ités)$");
+    if ((m = reg.exec(word)) != null && r2.endsWith(m[1])) {
+      //delete if in R2
+      word = word.slice(0, -m[1].length);
+      //if preceded by abil, delete if in R2, else replace by abl
+      if (word.endsWith("abil")){
+        if (r2.endsWith("abil" + m[1])) return {stem:word.slice(0, -4), next:false};
+        return {stem:word.slice(0, -2) + "l", next:false};
+      }
+      //if preceded by ic, delete if in R2, else replace by iqU
+      if (word.endsWith("ic")){
+        word = word.slice(0, -2);
+        if (r2.endsWith("ic" + m[1])) return {stem:word, next:false};
+        return {stem:word + "iqU", next:false};
+      }
+      //if preceded by iv, delete if in R2
+      if (r2.endsWith("iv" + m[1])) return {stem:word.slice(0, -2), next:false};
+      //otherwise
+      return {stem:word, next:false};
+    }
+
+
+
+
+    return {stem:word, next:true};
   }
 
   //TODO normalization of words
